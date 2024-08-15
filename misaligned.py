73b8970bf759d02d57b5a16b63b167268ba74650
MPM3KOR <manjularparmar@gmail.com>
2024-08-15 13:32:53 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,44 +1,59 @@
 
-# def print_color_map():
-#     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-#     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-#     for i, major in enumerate(major_colors):
-#         for j, minor in enumerate(minor_colors):
-#             print(f'{i * 5 + j} | {major} | {minor}')
-#     return len(major_colors) * len(minor_colors)
-
-
-
-# result = print_color_map()
-# assert(result == 25)
-# print("All is well (maybe!)\n")
-
-
-
-
-import logging
-
 def create_color_map():
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    major_colors = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+    minor_colors = ['Blue', 'Orange', 'Green', 'Brown', 'Slate']
     color_map = {}
     for MajorColors, major in enumerate(major_colors):
         for MinorColors, minor in enumerate(minor_colors):
-            index = MajorColors * len(minor_colors) + MinorColors
-            index+=1
+            index = MajorColors * len(minor_colors) + MinorColors #+ 1
             color_map[index] = (major, minor)
-            result_length = (len(major_colors) * len(minor_colors))
+    result_length = len(major_colors) * len(minor_colors)
     return color_map, result_length
+ 
+def print_color_map(create_color_map, logger):
+    color_map, result_length = create_color_map()
+    logger(color_map)
+    return result_length
+ 
+# Test Environment
+ 
+global color_map_arg
+ 
+def fakeLogger(color_map):
+    global color_map_arg
+    color_map_arg = color_map
+ 
+def testCases():
+    expected_map = {
+        1: ('White', 'Blue'),
+        2: ('White', 'Orange'),
+        3: ('White', 'Green'),
+        4: ('White', 'Brown'),
+        5: ('White', 'Slate'),
+        6: ('Red', 'Blue'),
+        7: ('Red', 'Orange'),
+        8: ('Red', 'Green'),
+        9: ('Red', 'Brown'),
+        10: ('Red', 'Slate'),
+        11: ('Black', 'Blue'),
+        12: ('Black', 'Orange'),
+        13: ('Black', 'Green'),
+        14: ('Black', 'Brown'),
+        15: ('Black', 'Slate'),
+        16: ('Yellow', 'Blue'),
+        17: ('Yellow', 'Orange'),
+        18: ('Yellow', 'Green'),
+        19: ('Yellow', 'Brown'),
+        20: ('Yellow', 'Slate'),
+        21: ('Violet', 'Blue'),
+        22: ('Violet', 'Orange'),
+        23: ('Violet', 'Green'),
+        24: ('Violet', 'Brown'),
+        25: ('Violet', 'Slate')
+    }
+    result_length = print_color_map(create_color_map, fakeLogger) 
+    assert(result_length == 25)
+    assert(color_map_arg == expected_map)
+ 
+testCases()
 
-
-
-def log_color_map(color_map):
-    for index, colors in color_map.items():
-        logging.info(f"{index } | {colors[0]} | {colors[1]}")
-    
-
-
-color_map,result_length = create_color_map()
-result = log_color_map(color_map)  
-assert(result_length == 24)
-print("All is well (maybe!)\n") 
